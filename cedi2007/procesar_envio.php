c47d4b9f3d0e6ff85386b810bb128b7546711e4f
srgblnch <srgblnch@gmail.com>
2012-05-08 09:09:39 +0200

    2007 article commited to the spanish crypto-seminar

---
+++ cedi2007/procesar_envio.php
@@ -0,0 +1,81 @@
+<html><head><title>CEDI - Congreso Español de Informática</title>
+
+
+<meta name="title" content="CEDI 2007">
+<meta http-equiv="Keywords" content="CEDI, cedi,
+Congreso Español de Informática, Congreso de Informática, Ingeniería Informática, Congreso de Ingeniería Informática">
+<meta http-equiv="Description" content="Congreso Español de Informática, Congreso de Informática, Ingeniería Informática, Congreso de Ingeniería Informática">
+<meta name="keyphrase" content="Congreso Español de Informática, Congreso de Informática, Ingeniería Informática, Congreso de Ingeniería Informática">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta content="MSHTML 6.00.2800.1276" name="GENERATOR">
+<link href="procesar_envio_files/estilos.css" rel="stylesheet" type="text/css"></head><body>
+<table align="center" border="0" cellpadding="0" cellspacing="0">
+  <tbody><tr> <td> 
+      <a href="http://www.congresocedi.es/2007/contenido.php?apartado=presentacion&amp;menu=1" target="_top"><img src="procesar_envio_files/cabecera.jpg" border="0"></a> 
+	</td>
+  </tr>
+  <tr> <td bgcolor="#cfc7d6" valign="top">
+
+
+<br>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; 
+<b>Datos introducidos : </b>
+</p><ul>
+<ul><li> Simposio: II Simposio sobre Seguridad Informática <p></p></li><li> Nombre del autor principal: Sergi</li><li> Apellidos del autor principal: Blanch i Torné</li><li> e-mail del autor principal: sblanch@alumnes.udl.cat</li><li> Afiliación: Universitat de Lleida  <p></p></li><li> Co-autor 2: Ramiro  Moreno Chiral</li><li> Co-autor 3:   </li><li> Co-autor 4:   </li><li> Co-autor 5:   </li><li> Co-autor 6:     <p></p></li><li> Título: Análisis del cifrado ElGamal de un módulo con curvas elípticas propuesto para el GnuPG  <p></p></li><li>
+Abstract: Desde marzo de 2007, el paquete de software libre Gnu Privacy
+Guard (GnuPG) incorpora en la versión de desarrollo soporte para
+criptografía con curvas elípticas. El código allí incluido fué
+realizado como proyecto final de carrera del primero de los autores.
+Sin embargo, el esquema de cifrado ElGamal sobre curvas elípticas
+presenta algunos problemas que se analizan y se plantea una solución
+alternativa. <p></p></li><li> Keywords: ElGamal curva elíptica GnuPG  <p></p></li><li> Fichero(nombre): AnalisisElGamalEccGnuPG.pdf <p></p></li><li> Fichero(tamaño): 162687 bytes <p></p></li><li> Temas:  Criptografía y Criptoanálisis.</li></ul>&nbsp; &nbsp; &nbsp; &nbsp; <h2> Artículo recibido correctamente. Todas las fuentes están incluidas y el documento no está encriptado.</h2><p>
+</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Identificador del artículo: <b>82</b>
+</p><p>&nbsp; &nbsp; &nbsp; &nbsp; Recuerde el siguiente password para posteriores referencias: <b>e67403c7</b>
+</p><p></p><p>
+<script> 
+function sendme() { 
+    window.open("","myNewWin","width=500,height=300,toolbar=0"); 
+    var a = window.setTimeout("document.formmails.submit();",500); 
+} 
+</script>
+</p><form name="formmails" method="post" action="http://atc.ugr.es/~pedro/pruebas/enviar_mails.php" target="myNewWin">
+<input name="cuantos" value="1" type="hidden"> 
+<input name="asunto" value="Trabajo recibido correctamente" type="hidden"> 
+<input name="email0" value="sblanch@alumnes.udl.cat" type="hidden"> 
+<input name="cuerpo0" value="Estimado/a Sergi,
+su artículo ha sido recibido correctamente por el sistema de recepción de trabajos del CEDI2007.
+
+Identificador artículo: 82
+Password para posteriores referencias: e67403c7 
+
+Datos del trabajo:
+Simposio: II Simposio sobre Seguridad Informática
+Título: Análisis del cifrado ElGamal de un módulo con curvas elípticas propuesto para el GnuPG
+Keywords: ElGamal curva elíptica GnuPG 
+
+Fichero(nombre): AnalisisElGamalEccGnuPG.pdf 
+Fichero(tamaño): 162687 bytes.
+Fichero(num.pags.): 7
+Todas las fuentes están incluidas y el documento no está encriptado.
+Temas:  Criptografía y Criptoanálisis.
+
+Abstract: Desde marzo de 2007, el paquete de software libre Gnu Privacy Guard (GnuPG) incorpora en la versión de desarrollo soporte para criptografía con curvas elípticas. El código allí incluido fué realizado como proyecto final de carrera del primero de los autores. Sin embargo, el esquema de cifrado ElGamal sobre curvas elípticas presenta algunos problemas que se analizan y se plantea una solución alternativa.
+
+Datos de los autores:
+Autor principal: Sergi Blanch i Torné
+Email: sblanch@alumnes.udl.cat
+Afilición: Universitat de Lleida
+Co-autores:
+Ramiro Moreno Chiral
+" type="hidden"> 
+<script language="JavaScript"><!-- 
+sendme(); 
+--></script> 
+<p><font size="-1">Conviene que habilite las ventanas emergentes para este sitio web de forma que se le puedan enviar los emails automáticamente.<br>Si no le llega un email de confirmación en breve, pulse <a href="#" onclick="javascript:sendme();">este enlace</a></font></p></form>
+
+
+</ul></td></tr>
+
+<tr><td><img src="procesar_envio_files/pie.jpg"></td></tr>
+</tbody></table>
+</body></html>
